5068fa49aceed55432913e662f60f967aa37e89a
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-02-23 14:16:32 +0100

    created html

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,5 @@
-// import React, { Component } from 'react';
-// import css from './Loader.module.css';
+import React, { Component } from 'react';
+import css from './Loader.module.css';
 
 export class Loader extends React.Component {
   render() {
--- src/components/Searchbar/Button/Button.jsx
+++ src/components/Searchbar/Button/Button.jsx
@@ -1,6 +1,6 @@
-// import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import css from './Button.module.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './Button.module.css';
 
 export class Button extends React.Component {
   render() {
